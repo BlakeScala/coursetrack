d90e1a7486bbfcab5165a5c1c5d581bb7702da55
Blake Scala <blakescala@gmail.com>
2016-08-12 10:36:38 -0700

    add results div with pictures and lorem ipsum filler, adjust div sizes in css

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,74 @@
 $(document).ready(function() {
-  $("#quiz").submit(function() {
+  $("#quiz").submit(function(event) {
+    event.preventDefault();
 
     var PHP = 0;
     var CSS = 0;
     var Java = 0;
 
-    var input1 = $("#mc1").val();
+    var answer1 = $("#mc1").val();
 
-    if (input1 === "1") {
+    if (answer1 === "1") {
       $(PHP += 1);
     }
-    else if (input1 === "2") {
-      $(CSS += 1);
-    }
-    else if (input1 === "3") {
+    else if (answer1 === "2") {
       $(Java += 1);
     }
+    else if (answer1 === "3") {
+      $(CSS += 1);
+    }
+
+    var answer2 = $("#mc2").val();
+
+    if (answer2 === "1") {
+      $(CSS += 2);
+    }
+    else if (answer2 ==="2") {
+      $(Java += 1);
+      $(PHP += 1);
+    }
+
+    var answer3 = $("#mc3").val();
+
+    if (answer3 === "1") {
+      $(PHP += 1);
+    }
+    else if (answer3 ==="2") {
+      $(Java += 1);
+    }
+
+    var answer4 = $("#mc4").val();
+
+    if (answer4 === "1") {
+      $(CSS += 1);
+      $(PHP += 1);
+    }
+    else if (answer4 ==="2") {
+      $(Java += 1);
+    }
+
+    var answer5 = $("#mc5").val();
+
+    if (answer5 === "1") {
+      $(Java += 1);
+      $(PHP += 1);
+    }
+    else if (answer5 ==="2") {
+      $(CSS += 3);
+    }
+
     alert(PHP)
-    alert(CSS)
     alert(Java)
+    alert(CSS)
+    
+    if (PHP > CSS && PHP > Java) {
+      $("#phpdiv").show();
+    }
+    else if (CSS > PHP && CSS > Java) {
+      $("#cssdiv").show();
+    }
+    else if (Java > PHP && Java > CSS) {
+      $("#javadiv").show();
+    }
   });
 });
